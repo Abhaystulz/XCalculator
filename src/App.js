838da2d3a49577f1a1f872ab8e96281c03d82a46
Abhay Kumar Yadav <abhaykumar9006@gmail.com>
2025-11-11 20:55:14 +0530

    Refactor calculator to use mathjs for evaluation

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { evaluate } from "mathjs"; 
 import "./App.css";
 
 const Calculator = () => {
@@ -13,10 +14,11 @@ const Calculator = () => {
     setInput("");
     setResult("");
   };
-  
+
   const handleCalculate = () => {
     try {
-      setResult(eval(input).toString());
+      const output = evaluate(input); 
+      setResult(output.toString());
     } catch (error) {
       setResult("Error");
     }
@@ -29,60 +31,28 @@ const Calculator = () => {
       {result && <div className="result">{result}</div>}
       <div className="buttons">
         <div className="button-row">
-          <button className="button" onClick={() => handleInput("7")}>
-            7
-          </button>
-          <button className="button" onClick={() => handleInput("8")}>
-            8
-          </button>
-          <button className="button" onClick={() => handleInput("9")}>
-            9
-          </button>
-          <button className="button" onClick={() => handleInput("+")}>
-            +
-          </button>
+          <button className="button" onClick={() => handleInput("7")}>7</button>
+          <button className="button" onClick={() => handleInput("8")}>8</button>
+          <button className="button" onClick={() => handleInput("9")}>9</button>
+          <button className="button" onClick={() => handleInput("+")}>+</button>
         </div>
         <div className="button-row">
-          <button className="button" onClick={() => handleInput("4")}>
-            4
-          </button>
-          <button className="button" onClick={() => handleInput("5")}>
-            5
-          </button>
-          <button className="button" onClick={() => handleInput("6")}>
-            6
-          </button>
-          <button className="button" onClick={() => handleInput("-")}>
-            -
-          </button>
+          <button className="button" onClick={() => handleInput("4")}>4</button>
+          <button className="button" onClick={() => handleInput("5")}>5</button>
+          <button className="button" onClick={() => handleInput("6")}>6</button>
+          <button className="button" onClick={() => handleInput("-")}>-</button>
         </div>
         <div className="button-row">
-          <button className="button" onClick={() => handleInput("1")}>
-            1
-          </button>
-          <button className="button" onClick={() => handleInput("2")}>
-            2
-          </button>
-          <button className="button" onClick={() => handleInput("3")}>
-            3
-          </button>
-          <button className="button" onClick={() => handleInput("*")}>
-            *
-          </button>
+          <button className="button" onClick={() => handleInput("1")}>1</button>
+          <button className="button" onClick={() => handleInput("2")}>2</button>
+          <button className="button" onClick={() => handleInput("3")}>3</button>
+          <button className="button" onClick={() => handleInput("*")}>*</button>
         </div>
         <div className="button-row">
-          <button className="button" onClick={handleClear}>
-            C
-          </button>
-          <button className="button" onClick={() => handleInput("0")}>
-            0
-          </button>
-          <button className="button" onClick={handleCalculate}>
-            =
-          </button>
-          <button className="button" onClick={() => handleInput("/")}>
-            /
-          </button>
+          <button className="button" onClick={handleClear}>C</button>
+          <button className="button" onClick={() => handleInput("0")}>0</button>
+          <button className="button" onClick={handleCalculate}>=</button>
+          <button className="button" onClick={() => handleInput("/")}>/</button>
         </div>
       </div>
     </div>
